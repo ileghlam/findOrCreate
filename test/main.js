ac3940f9b30e64157e4f4b822a50cc248df7e161
Ishak LEGHLAM <ileghlam@e2r3p2.42.fr>
2017-02-08 02:50:51 +0100

    Create first test : if findOrCreate is a function

--- test/main.js
+++ test/main.js
@@ -1,11 +1,50 @@
-var should = require('should');
-var sleepsort = require('../lib/main');
+import chai from 'chai';
+/* --------------------------------------------Require------------------------------------------ */
+import mongoose from 'mongoose';
+import assert from 'assert';
+import findOrCreate from '../lib/main';
 
-describe('sleepsort', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-            var result = sleepsort();
-            result.should.eql([]);
+/* --------------------------------------------Init Schema-------------------------------------- */
+const { expect } = chai;
+const Schema = mongoose.Schema;
+
+const testSchema = new Schema({
+    name: { type: String },
+});
+
+testSchema.plugin(findOrCreate);
+
+const test = mongoose.model('test', testSchema);
+
+/* --------------------------------------Connect to database------------------------------------ */
+mongoose.connect('mongodb://82.196.14.126:27017/crypta');
+// mongoose.connection.on('connected', function () {  
+//     console.log('Mongoose default connection open to ' + 'mongodb://82.196.14.126:27017/crypta');
+// }); 
+
+// // If the connection throws an error
+// mongoose.connection.on('error',function (err) {  
+//     console.log('Mongoose default connection error: ' + err);
+// }); 
+
+// // When the connection is disconnected
+// mongoose.connection.on('disconnected', function () {  
+//     console.log('Mongoose default connection disconnected'); 
+// });
+
+/* ------------------------------------------START TEST----------------------------------------- */
+describe('#findOrCreate()', () => {
+
+    before(done => {
+        console.log('pourquoi');
+        const grapefruit = new test({
+            name: 'Grapefruit',
         });
+        grapefruit.save(err => done())
     });
+
+    it('the static method findOrCreate is added to models', () => {
+        expect(typeof test.findOrCreate).to.equal('function')
+    })
+
 });
