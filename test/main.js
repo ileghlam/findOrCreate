06549a216e4314ec73b9cccd7623adc3f4aff7b0
Ishak LEGHLAM <ileghlam@e2r4p3.42.fr>
2017-02-08 15:46:14 +0100

    New test : Create a new elem in database

--- test/main.js
+++ test/main.js
@@ -3,6 +3,26 @@ import chai from 'chai';
 import mongoose from 'mongoose';
 import findOrCreate from '../lib/main';
 
+const { it, describe } = global;
+
+
+
+/* --------------------------------------Connect to database------------------------------------ */
+mongoose.connect('mongodb://82.196.14.126:27017/crypta');
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose default connection open to mongodb://82.196.14.126:27017/crypta');
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose default connection error: ${err}`);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose default connection disconnected');
+});
+
 /* --------------------------------------------Init Schema-------------------------------------- */
 const { expect } = chai;
 const Schema = mongoose.Schema;
@@ -17,36 +37,33 @@ testSchema.plugin(findOrCreate);
 
 const Test = mongoose.model('test', testSchema);
 
-/* --------------------------------------Connect to database------------------------------------ */
-mongoose.connect('mongodb://82.196.14.126:27017/crypta');
-// mongoose.connection.on('connected', function () {
-//     console.log('Mongoose default connection open to ' + 'mongodb://82.196.14.126:27017/crypta');
-// });
-
-// // If the connection throws an error
-// mongoose.connection.on('error',function (err) {
-//     console.log('Mongoose default connection error: ' + err);
-// });
-
-// // When the connection is disconnected
-// mongoose.connection.on('disconnected', function () {
-//     console.log('Mongoose default connection disconnected');
-// });
-
 /* ------------------------------------------START TEST----------------------------------------- */
 describe('#findOrCreate()', () => {
-    before((done) => {
-        const test = new Test({
-            name: 'test',
-        });
-        test.save(err => done());
-    });
+    // before((done) => {
+    //     const test = new Test({
+    //         name: 'test',
+    //     });
+    //     test.save(err => done());
+    // });
 
     it('should add method findOrCreate to models', () => {
         expect(typeof Test.findOrCreate).to.equal('function');
     });
 
-    it('should ')
+    it('should create a new elem in database', () => {
+        Test.findOrCreate({ name: 'mango' })
+            .then((mec) => {
+                console.log('4');
+                expect(mec.name).to.equal('mango');
+            }).catch(console.error);
+    });
+    //   it("should create the object if it doesn't exist", function(done) {
+    //     Test.findOrCreate({name: 'test'}, function(err, click) {
+    //     click.name.should.eql('test');
+
+    //     });
+    // });
+
 });
 
 /* ----------------------------------------Delete Database-------------------------------------- */
