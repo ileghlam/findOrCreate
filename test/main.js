c913942ea82a77c4486155c9c30fe19f59057e10
Ishak LEGHLAM <ileghlam@e2r3p4.42.fr>
2017-02-12 01:33:39 +0100

    New Tests

--- test/main.js
+++ test/main.js
@@ -164,6 +164,294 @@ describe('#findOrCreate()', () => {
         })
         .catch(done);
     });
+
+    // Test 11
+    it('should create document with create option has true and data', (done) => {
+        Test.findOrCreate({ name: 'bb8' }, { school: 420 }, { create: true })
+        .then((doc) => {
+            doc.created.should.equal(true);
+            doc.result.school.should.equal(420);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 12
+    it('should create document with create option has false and data', (done) => {
+        Test.findOrCreate({ name: 'bb9' }, { parent: 'jupiter' }, { create: false })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 13
+    it('should create document with create option has true and data', (done) => {
+        Test.findOrCreate({ name: 'bb9' }, { parent: 'jupiter' }, { create: true })
+        .then((doc) => {
+            doc.created.should.equal(true);
+            doc.result.parent.should.equal('jupiter');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 14
+    it('should find the document with everything the document contains', (done) => {
+        Test.findOrCreate({ name: 'bb9', parent: 'jupiter' })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.parent.should.equal('jupiter');
+            doc.result.name.should.equal('bb9');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 15
+    it('should find the document with more than the document contains', (done) => {
+        Test.findOrCreate({ name: 'bb9', parent: 'jupiter', school: 42 }, { create: false })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 16
+    it('should create document only with search and options', (done) => {
+        Test.findOrCreate({ name: 'bb10', parent: 'valide', school: 77 })
+        .then((doc) => {
+            doc.created.should.equal(true);
+            doc.result.name.should.equal('bb10');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 17
+    it('should update multiple arguments', (done) => {
+        Test.findOrCreate({ name: 'bb10' }, { parent: 'eiffel tower', school: 42 }, { upsert: true })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('bb10');
+            doc.result.parent.should.equal('eiffel tower');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 18
+    it('should update nothing', (done) => {
+        Test.findOrCreate({ name: 'bb10' }, {}, { upsert: true })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('bb10');
+            doc.result.parent.should.equal('eiffel tower');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 19
+    it('should update document with upsert has false', (done) => {
+        Test.findOrCreate({ name: 'bb10' }, { parent: 'ahah', school: 99 }, { upsert: false })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('bb10');
+            doc.result.parent.should.equal('eiffel tower');
+            doc.result.school.should.equal(42);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 20
+    it('should create document with upsert has true and create has true', (done) => {
+        Test.findOrCreate(
+            { name: 'one' },
+            {
+                school: 420,
+                number: 321,
+                parent: 'old',
+            },
+            {
+                upsert: true,
+                create: true,
+            })
+        .then((doc) => {
+            doc.created.should.equal(true);
+            doc.result.name.should.equal('one');
+            doc.result.school.should.equal(420);
+            doc.result.number.should.equal(321);
+            doc.result.parent.should.equal('old');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 21
+    it('should create document with upsert has false and create has false', (done) => {
+        Test.findOrCreate(
+            { name: 'two' },
+            {
+                school: 420,
+                number: 321,
+                parent: 'old',
+            },
+            {
+                upsert: false,
+                create: false,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 22
+    it('should create document with upsert has true and create has false', (done) => {
+        Test.findOrCreate(
+            { name: 'three' },
+            {
+                school: 420,
+                number: 321,
+                parent: 'old',
+            },
+            {
+                upsert: true,
+                create: false,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 23
+    it('should create document with upsert has false and create has true', (done) => {
+        Test.findOrCreate(
+            { name: 'four' },
+            {
+                school: 420,
+                number: 321,
+                parent: 'old',
+            },
+            {
+                upsert: true,
+                create: true,
+            })
+        .then((doc) => {
+            doc.created.should.equal(true);
+            doc.result.name.should.equal('four');
+            doc.result.school.should.equal(420);
+            doc.result.number.should.equal(321);
+            doc.result.parent.should.equal('old');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 24
+    it('should update document with upsert has true and create has true', (done) => {
+        Test.findOrCreate(
+            { name: 'one' },
+            {
+                name: 'two',
+                school: 46,
+                number: 42,
+                parent: 'paste',
+            },
+            {
+                upsert: true,
+                create: true,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('two');
+            doc.result.school.should.equal(46);
+            doc.result.number.should.equal(42);
+            doc.result.parent.should.equal('paste');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 25
+    it('should update document with upsert has false and create has false', (done) => {
+        Test.findOrCreate(
+            { name: 'two' },
+            {
+                school: 420,
+                number: 99,
+                parent: 'paste',
+            },
+            {
+                upsert: false,
+                create: false,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('two');
+            doc.result.school.should.equal(46);
+            doc.result.number.should.equal(42);
+            doc.result.parent.should.equal('paste');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 26
+    it('should update document with upsert has true and create has false', (done) => {
+        Test.findOrCreate(
+            { name: 'two' },
+            {
+                name: 'one',
+                school: 420,
+                number: 99,
+                parent: 'paste',
+            },
+            {
+                upsert: true,
+                create: false,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('one');
+            doc.result.school.should.equal(420);
+            doc.result.number.should.equal(99);
+            doc.result.parent.should.equal('paste');
+            done(null);
+        })
+        .catch(done);
+    });
+
+    // Test 27
+    it('should update document with upsert has false and create has true', (done) => {
+        Test.findOrCreate(
+            { name: 'one' },
+            {
+                school: 88,
+                number: 0,
+                parent: 'bye',
+            },
+            {
+                upsert: false,
+                create: true,
+            })
+        .then((doc) => {
+            doc.created.should.equal(false);
+            doc.result.name.should.equal('one');
+            doc.result.school.should.equal(420);
+            doc.result.number.should.equal(99);
+            doc.result.parent.should.equal('paste');
+            done(null);
+        })
+        .catch(done);
+    });
 });
 /* ----------------------------------------Delete Database-------------------------------------- */
 after(done => {
